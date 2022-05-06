ac17a18f9957d23a93dca0fcbd6a2677d1e20b83
AN <an.nguyen.thein.ho@gmail.com>
2022-05-06 09:07:05 -0700

    update

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -153,6 +153,8 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Recurse on the children and then print the value in the current node
 	public void postOrder() {
 		postOrderRecurse(root); 
+		System.out.println("postOrder test commit");
+
 	}
 	
 	public void postOrderRecurse(BSTNode<T> node) {
